21ecaa057aae1d5f851b96868773849c42700a0d
dsn5 <dsn5@sfu.ca>
2022-02-26 11:52:11 -0800

    Responsive Navbar Created, Layout Fixes

--- src/Layout.js
+++ src/Layout.js
@@ -1,64 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import { MdMenu } from 'react-icons/all'
+import React from 'react'
+import { Outlet} from 'react-router-dom'
 
 import './styles/Main.css'
-import logo from './assets/images/bgc_logo.png'
-
-const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window
-    return {width, height}
-}
+import Navbar from './components/Navbar'
 
 const Layout = () => {
-
-    const [windowWidth, setWindowWidth] = useState(getWindowDimensions().width)
-
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWindowWidth(getWindowDimensions().width)
-        }
-
-        window.addEventListener('resize', handleWindowResize)
-    }, [])
 
     return (
         <div>
 
-            <div className={'outer'}>
+            <Navbar />
 
-                <header className={'header'}>
+            <div className={'contentContainer'}>
 
-                    <ul>
-
-                        <li>
-                            <a href='/'>
-                                <img src={logo} alt={'Logo'} className={'logo'} />
-                            </a>
-                        </li>
-
-                        <div className={'navigationContent'}>
-                            {(windowWidth > 480) ?
-                                <div>
-                                    <li><a href={'/'}>Home</a></li>
-                                    <li><a href={'/search'}>Search</a></li>
-                                    <li><a href={'/dashboard'}>Dashboard</a></li>
-                                </div>
-                                :
-                                <div>
-                                    <a href={'/'}><MdMenu color={'white'} size={32} /></a>
-                                </div>
-                            }
-                        </div>
-
-                    </ul>
-
-                </header>
-
-                <div className={'contentContainer'}>
-                    <Outlet />
-
-                </div>
+                <Outlet />
 
             </div>
 
