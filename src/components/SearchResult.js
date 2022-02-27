2ecc9ace872cc15108a6a0447ac0fe839cdb055b
Diljodh Nahal <47072341+DiljodhNahal@users.noreply.github.com>
2022-02-26 16:56:51 -0800

    Initial version of search page complete, more layout fixes (#9)
    Co-authored-by: dsn5 <dsn5@sfu.ca>

---
+++ src/components/SearchResult.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import '../styles/components/SearchResult.css'
+
+const SearchResult = ({ result }) => {
+
+
+    return (
+
+        <div className={'resultCol'}>
+
+            <div className={'searchResult'}>
+
+                <div style={{ fontSize: 24 }}>{result.name}</div>
+
+                <img
+                    src={'https://cdn.thewirecutter.com/wp-content/media/2020/10/beginnerdslr2020-2048px-9793.jpg?auto=webp&quality=60&crop=1.91:1&width=1200'}
+                    alt={'Display Picture'}
+                    style={{height: '200px'}}
+                />
+
+                <div style={{ padding: 8 }}>Barcode: {result.barcode}</div>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default SearchResult
