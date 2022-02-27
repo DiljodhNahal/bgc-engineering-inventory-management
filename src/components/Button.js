f330942310ec0a3421d9be3c5a429fb5842ba4f8
DiljodhNahal <diljodhnahal@hotmail.com>
2022-02-27 14:57:28 -0800

    Updated Dashboard Styles & Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 import '../styles/components/Button.css'
 
-const Button = ({ children, onClick }) => {
+const Button = ({ children, onClick, size }) => {
+
+    let sizeClass = 'button-md'
+    if (size) {
+        switch (size.toLowerCase()) {
+            case 'large':
+                sizeClass = 'button-lg'
+                break
+            case 'small':
+                sizeClass = 'button-sm'
+                break
+        }
+    }
 
     return (
 
-        <div onClick={onClick} className={'button'}>
+        <div onClick={onClick} className={`button ${sizeClass}`}>
             {children}
         </div>
 
