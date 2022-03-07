6cdc00f053133b717f24e3a762655c4f8bfebf59
DiljodhNahal <diljodhnahal@hotmail.com>
2022-03-06 21:10:22 -0800

    Fixed authentication API request, moved request to layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet} from 'react-router-dom'
 
 import './styles/Main.css'
 import Navbar from './components/Navbar'
 
 const Layout = () => {
+
+    const [authentication, setAuthentication] = useState()
+
+    useEffect(() => {
+        fetch(`/api/autheticated`)
+            .then(response => response.json())
+            .then(data => {
+                setAuthentication(data)
+            })
+
+    })
 
     return (
         <div>
