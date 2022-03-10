e43d53dbdf0ccd1c683c20de27079faa16ee2697
Ajaipaul Cheema <ajaipaul_cheema@hotmail.com>
2022-03-09 18:48:59 -0800

    Cleaned up and reformatted code

--- server/index.js
+++ server/index.js
@@ -1,200 +1,211 @@
-const express = require('express')
-const pool = require('./db')
-const bodyParser = require('body-parser')
-const path = require('path')
-const pino = require('express-pino-logger')()
-const bcrypt = require("bcrypt")
+const express = require("express");
+const pool = require("./db");
+const bodyParser = require("body-parser");
+const path = require("path");
+const pino = require("express-pino-logger")();
+const bcrypt = require("bcrypt");
 const passport = require("passport");
-const initializePassport = require('./loginConfig')
-const session = require('express-session')
+const initializePassport = require("./loginConfig");
+const session = require("express-session");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-initializePassport(passport)
+initializePassport(passport);
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(pino)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.set('views', path.join(__dirname, 'src/pages'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "src/pages"));
 
 app.use(
-    session({
-        secret: 'hidden',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: false
-        }
-    })
-)
+  session({
+    secret: "hidden",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+    },
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global Error Handling
-const asyncHandler = fn => (req, res, next) => {
-    return Promise
-        .resolve(fn(req, res, next))
-        .catch(next);
-}
+const asyncHandler = (fn) => (req, res, next) => {
+  return Promise.resolve(fn(req, res, next)).catch(next);
+};
 
 // Endpoints
-app.get('/api/search', asyncHandler(async (req, res) => {
-
+app.get(
+  "/api/search",
+  asyncHandler(async (req, res) => {
     try {
-        // Build SQL String
-        let queryString = `SELECT * FROM equipment`
-        let queries = 0
+      // Build SQL String
+      let queryString = `SELECT * FROM equipment`;
+      let queries = 0;
 
-        const updateString = (key, value) => {
-
-            // Add Operators To Query String Dependant On Position
-            if (queries === 0) {
-                queryString += ' WHERE'
-            } else {
-                queryString += ' AND'
-            }
-
-            // Search or Filter Depending On key
-            if (key === 'name') {
-                queryString += ` SIMILARITY(name, '${value}') > 0.4`
-            } else {
-                // Add Key To Query String
-                if (key === 'id') {
-                    queryString += ` ${key} =`
-                } else {
-                    queryString += ` ${key} ilike`
-                }
-
-                // Add Value To Query String
-                if (Number.isInteger(value)) {
-                    queryString += ` ${value}`
-                } else {
-                    queryString += ` '${value}'`
-                }
-            }
-
-            queries += 1
+      const updateString = (key, value) => {
+        // Add Operators To Query String Dependant On Position
+        if (queries === 0) {
+          queryString += " WHERE";
+        } else {
+          queryString += " AND";
         }
 
-        // Filters
-        for (let key in req.query)
-            updateString(key, req.query[key])
+        // Search or Filter Depending On key
+        if (key === "name") {
+          queryString += ` SIMILARITY(name, '${value}') > 0.4`;
+        } else {
+          // Add Key To Query String
+          if (key === "id") {
+            queryString += ` ${key} =`;
+          } else {
+            queryString += ` ${key} ilike`;
+          }
 
-        // Query Results
-        let results = await pool.query(queryString)
+          // Add Value To Query String
+          if (Number.isInteger(value)) {
+            queryString += ` ${value}`;
+          } else {
+            queryString += ` '${value}'`;
+          }
+        }
 
-        // Build Response
-        let response = { count: results.rowCount, results: results.rows }
+        queries += 1;
+      };
 
-        res.send(response)
+      // Filters
+      for (let key in req.query) updateString(key, req.query[key]);
+
+      // Query Results
+      let results = await pool.query(queryString);
+
+      // Build Response
+      let response = { count: results.rowCount, results: results.rows };
+
+      res.send(response);
     } catch (exception) {
-        throw new Error(exception.message)
+      throw new Error(exception.message);
     }
+  })
+);
 
-}))
-
-app.post('/api/signup', asyncHandler(async (req, res) => {
-
+app.post(
+  "/api/signup",
+  asyncHandler(async (req, res) => {
     try {
-        let { email, password, accountType } = req.body
+      let { email, password, accountType } = req.body;
 
-        let queryString = `SELECT * FROM users WHERE email = $1`
-        let response
+      let queryString = `SELECT * FROM users WHERE email = $1`;
+      let response;
 
-        // Salt To Be Used With Password
-        const SALT = 16
+      // Salt To Be Used With Password
+      const SALT = 16;
 
-        // Hashing The Password Entered
-        bcrypt.hash(password, SALT, (err, hash) => {
-            if (err) {
-                res.status(500).send('An Unexpected Error Occurred')
-                return
+      // Hashing The Password Entered
+      bcrypt.hash(password, SALT, (err, hash) => {
+        if (err) {
+          res.status(500).send("An Unexpected Error Occurred");
+          return;
+        }
+        pool.query(queryString, [email], (err, results) => {
+          let queryInsertString = `INSERT INTO users("email", "password", "accountType") VALUES($1, $2, $3) RETURNING id, password`;
+
+          if (err) {
+            res.status(500).send("An Unexpected Error Occurred");
+            return;
+          }
+
+          // Email Exists
+          if (results.rows.length > 0) {
+            res.status(409).send("Email Already Exists");
+            return;
+          }
+
+          // Now Register User
+          pool.query(
+            queryInsertString,
+            [email, hash, accountType],
+            (err, result) => {
+              if (err) {
+                response = {
+                  message: "An Unexpected Error Occurred",
+                  status: 500,
+                };
+                res.send(response);
+                return;
+              }
+              res.status(201).send("Account Created");
             }
-            pool.query(
-                queryString, [email], (err, results) => {
-                    let queryInsertString = `INSERT INTO users("email", "password", "accountType") VALUES($1, $2, $3) RETURNING id, password`
-
-                    if (err) {
-                        res.status(500).send('An Unexpected Error Occurred')
-                        return
-                    }
-
-                    // Email Exists
-                    if (results.rows.length > 0) {
-                        res.status(409).send('Email Already Exists')
-                        return
-                    }
-
-                    // Now Register User
-                    pool.query(
-                        queryInsertString, [email, hash, accountType], (err, result) => {
-                            if (err) {
-                                response = { message: 'An Unexpected Error Occurred', status: 500 }
-                                res.send(response)
-                                return
-                            }
-                            res.status(201).send('Account Created')
-                        }
-                    )
-                }
-            )
-        })
+          );
+        });
+      });
     } catch (exception) {
-        throw new Error(exception.message)
+      throw new Error(exception.message);
     }
-}))
+  })
+);
 
 // Validates The User And Redirects Them
-app.post("/api/auth", passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/auth",
-    })
-)
+app.post(
+  "/api/auth",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth",
+  })
+);
 
 // Checks If User Is Logged In
-app.get('/api/autheticated', asyncHandler(async (req, res) => {
+app.get(
+  "/api/autheticated",
+  asyncHandler(async (req, res) => {
     let response = {
-        status: req.isAuthenticated()
-    }
-    if (req.isAuthenticated())
-        response.user = req.user
+      status: req.isAuthenticated(),
+    };
+    if (req.isAuthenticated()) response.user = req.user;
 
-    res.send(response)
-}))
+    res.send(response);
+  })
+);
 
 // Logs Out The User
-app.post('/api/logout', (req, res) => {
-    req.logout()
-    .then(() => {
-        return req.isAuthenticated()
-    })
-})
+app.post("/api/logout", (req, res) => {
+  req.logout().then(() => {
+    return req.isAuthenticated();
+  });
+});
 
 app.post("/api/upload", async (req, res) => {
-    try {
-        const { name, description, color, serialNumber, price, purchaseDate, barcode } = req.body
-        const newItem = await pool.query(
-            'INSERT INTO equipment (name, description, color, "serialNumber", price, "purchaseDate", barcode) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-            [name, description, color, serialNumber, price, purchaseDate, barcode]
-        )
-        res.json(newItem.rows[0])
-    } catch (err) {
-        console.error(err.message)
-        throw new Error(exception.message)
-    }
-})
+  try {
+    const {
+      name,
+      description,
+      color,
+      serialNumber,
+      price,
+      purchaseDate,
+      barcode,
+    } = req.body;
+    await pool.query(
+      'INSERT INTO equipment (name, description, color, "serialNumber", price, "purchaseDate", barcode) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      [name, description, color, serialNumber, price, purchaseDate, barcode]
+    );
+  } catch (err) {
+    console.error(err.message);
+    throw new Error(exception.message);
+  }
+});
 
 // Global Error Handling
 app.use(function (err, req, res, next) {
-    res.status(500).send(err.message)
-})
-
+  res.status(500).send(err.message);
+});
 
 app.listen(3001, () =>
-    console.log('Express server is running on localhost:3001')
-)
+  console.log("Express server is running on localhost:3001")
+);
--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from './Layout'
-import Home from './pages/Home'
-import Search from './pages/Search'
-import Dashboard from './pages/Dashboard'
-import Auth from './pages/Auth'
-import SignUp from './pages/SignUp'
-import Error from './pages/Error'
-import Create from './pages/Create'
-import EquipmentInfo from './pages/EquipmentInfo'
-import Modal from './components/Modal'
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import SignUp from "./pages/SignUp";
+import Error from "./pages/Error";
+import Create from "./pages/Create";
+import EquipmentInfo from "./pages/EquipmentInfo";
 
 export default function App() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path={"search"} element={<Search />} />
-                    <Route path={"dashboard"} element={<Dashboard />} />
-                    <Route path={"auth"} element={<Auth />} />
-                    <Route path={"signup"} element={<SignUp />} />
-                    <Route path={"create"} element={<Create />} />
-                    <Route path={"info/:id"} element={<EquipmentInfo />} />
-                </Route>
-                <Route path={"/error/:code"} element={<Error />} />
-            </Routes>
-        </BrowserRouter>
-    )
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={"search"} element={<Search />} />
+          <Route path={"dashboard"} element={<Dashboard />} />
+          <Route path={"auth"} element={<Auth />} />
+          <Route path={"signup"} element={<SignUp />} />
+          <Route path={"create"} element={<Create />} />
+          <Route path={"info/:id"} element={<EquipmentInfo />} />
+        </Route>
+        <Route path={"/error/:code"} element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
