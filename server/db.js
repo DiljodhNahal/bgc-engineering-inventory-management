b3ca6fafcecc43c0bc288d59d7ac122506f2296c
Diljodh Nahal <47072341+DiljodhNahal@users.noreply.github.com>
2022-03-10 15:05:12 -0800

    Development (#31)
    * Updated Dashboard Styles & Button Component (#18)
    
    * Authentication for Admin (#19)
    
    * Modal favicon (#20)
    
    * Updated Dashboard Styles & Button Component
    
    * Error page added, favicon added, modal styles fixed
    
    * sign up forms complete (#21)
    
    Co-authored-by: inder <isr@sfu.ca>
    
    * Signup update (#22)
    
    * sign up forms complete
    
    * sign up updated
    
    Co-authored-by: inder <isr@sfu.ca>
    
    * Auth js rework (#23)
    
    * Updated Dashboard Styles & Button Component
    
    * Reorganized Auth.js, prepared for connection to backend
    
    * Authentication for signup and login basics compelete (#24)
    
    Co-authored-by: Haris Ahmad <HarisAhmad04@hotmail.com>
    
    * Fixed authentication API request, moved request to layout.js
    
    * Fixed authentication API request, moved request to layout.js (#25)
    
    * Added Login, Fixed Registration, Conditional Rendering
    
    * Pre-Iteration 1
    
    * Logout endpoint created (#28)
    
    * Added logout functionality
    
    * Potential fix to SSL issue on heroku
    
    Co-authored-by: Haris Ahmad <88016633+HarisAhmad16@users.noreply.github.com>
    Co-authored-by: inder <isr@sfu.ca>
    Co-authored-by: Rangler34 <98732209+Rangler34@users.noreply.github.com>
    Co-authored-by: Haris Ahmad <HarisAhmad04@hotmail.com>

--- server/db.js
+++ server/db.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
 })
 
 module.exports = pool
