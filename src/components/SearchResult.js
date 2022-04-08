0282263ce1cfd5011eff60b49affc01a527ad093
Diljodh Nahal <47072341+DiljodhNahal@users.noreply.github.com>
2022-04-07 19:41:06 -0700

    Development (#65)
    * Updated package.json with dev script
    
    * Fixed development script
    
    * Solved 431 error
    
    * Whoops...
    
    * Manage User page set up
    
    * Created announcement button; prepared home page
    
    * Edit functionality and table for showing items
    
    * Table layout fixed
    
    * update
    
    * Added Alert Component
    
    * EquipmentInfo finished all attributes added & work
    
    * Managing User functional
    
    * added items to table and edit and delete items
    
    * worked on requests ui
    
    * fixed create issue
    
    * fixed create
    
    * corrected css
    
    * Employee accounts can now be made
    
    * fixed console issue
    
    * reformatted react fragment
    
    * Table issues fixed
    
    * Modal made for new accouncements, table shows them
    
    * Added Barcode Scanner
    
    * API endpoint fix
    
    * added queries
    
    * updated create table attributes
    
    * Announcement UI and Bug fix
    
    * pending requests
    
    * Pending Request and Signed-Out Functional
    
    * Migration fix
    
    * Deny functionality and update working
    
    * New & Manage announcements complete
    
    * button fix
    
    * fixed create border
    
    * Cleaned up announcements pages and css
    
    * return functionality working
    
    * drop down implemented and signed out edit works
    
    * added edit and delete based on account type
    
    * pushed
    
    * Delete working
    
    * deleted manage inventory
    
    * Admin request no pending
    
    * fixed delete annoucement looks
    
    * request style changed
    
    * potentially final commit
    
    * small changes to announcements and equipment info page
    
    * add conditional rendering
    
    Co-authored-by: Haris Ahmad <HarisAhmad04@hotmail.com>
    Co-authored-by: tanjo <tsh7@sfu.ca>
    Co-authored-by: Ajaipaul Cheema <ajaipaul_cheema@hotmail.com>
    Co-authored-by: Haris Ahmad <88016633+HarisAhmad16@users.noreply.github.com>
    Co-authored-by: Ajaipaul Cheema <96704215+Ajaipaul-Cheema@users.noreply.github.com>
    Co-authored-by: inder <isr@sfu.ca>
    Co-authored-by: Tanjodh Hayer <77908541+KingObito@users.noreply.github.com>
    Co-authored-by: Inderpreet Rangi <98732209+Rangler34@users.noreply.github.com>

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -19,12 +19,6 @@ const SearchResult = ({ result }) => {
 
                 <div style={{ fontSize: 24 }}>{result.name}</div>
 
-                <img
-                    src={'https://cdn.thewirecutter.com/wp-content/media/2020/10/beginnerdslr2020-2048px-9793.jpg?auto=webp&quality=60&crop=1.91:1&width=1200'}
-                    alt={'Display'}
-                    style={{height: '200px'}}
-                />
-
                 <div style={{ padding: 8 }}>Barcode: {result.barcode}</div>
 
             </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/pages/Dashboard.css'
+import { reactLocalStorage } from 'reactjs-localstorage'
 import Button from '../components/Button'
 import {useNavigate} from 'react-router-dom'
 
@@ -9,23 +10,30 @@ const Dashboard = () => {
 
     const navigation = useNavigate()
 
+    let accountType = parseInt(reactLocalStorage.get('accountType'))
+
     return (
         <div>
             <div className={'btn-container'}>
+                {accountType !== 0 &&
                 <div className={'btn-group'}>
                     <Button onClick={() => navigation('/create')}>Create Inventory</Button>
                     <Button onClick={() => navigation('/requests')}>Pending Requests</Button>
                     <Button onClick={() => navigation('/signedoutitems')}>Signed Out Items</Button>
                 </div>
-                <div className={'btn-group'}>
-                    <Button onClick={() => navigation('/signup')}>Create Users</Button>
-                    <Button onClick={() => navigation('/manageuser')}>Manage Users</Button>
-                    <Button onClick={() => navigation('/locations')}>Manage Locations</Button>
+                }
+                {accountType === 2 &&
+                <div>
+                    <div className={'btn-group'}>
+                        <Button onClick={() => navigation('/signup')}>Create Users</Button>
+                        <Button onClick={() => navigation('/manageuser')}>Manage Users</Button>
+                        <Button onClick={() => navigation('/announcements')}>New Announcements</Button>
+                    </div>
+                    <div className={'btn-group'}>
+                        <Button onClick={() => navigation('/announcement')}>Manage Announcements</Button>
+                    </div>
                 </div>
-                <div className={'btn-group'}>
-                    <Button onClick={() => navigation('/announcements')}>New Announcements</Button>
-                    <Button onClick={() => navigation('/announcement')}>Manage Announcements</Button>
-                </div>
+                }
 
             </div>
             
